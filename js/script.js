27373ecf2fff52bef3f706012d3a5b23d5e7e76b
Dihep <dihep.demayo@gmail.com>
2024-04-29 23:56:25 +0200

    M1- Lista spesa creata.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
 // Creazione variabili
+var listaSpesa = ["Nutella", "Marmellata", "Pane", "Pasta", "Sugo", "Maionese", "Asparagi", "Mascarpone", "Pere", "Mele", "Carote", "Prosciutto", "Jogurt", "Passata di pomodoro", "Latte", "Cioccolata"];
