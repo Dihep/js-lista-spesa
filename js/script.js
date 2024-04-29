9fb92da4f33a949b4cb9f5720217a5a2a8c7a38d
Dihep <dihep.demayo@gmail.com>
2024-04-30 00:00:05 +0200

    M1- Stampa con "for".

--- js/script.js
+++ js/script.js
@@ -1,2 +1,7 @@
 // Creazione variabili
-var listaSpesa = ["Nutella", "Marmellata", "Pane", "Pasta", "Sugo", "Maionese", "Asparagi", "Mascarpone", "Pere", "Mele", "Carote", "Prosciutto", "Jogurt", "Passata di pomodoro", "Latte", "Cioccolata"];
+var listaSpesa = ["Nutella", "Marmellata", "Pane", "Pasta", "Sugo", "Maionese", "Asparagi", "Mascarpone", "Pere", "Mele", "Carote", "Prosciutto", "Jogurt", "Passata di pomodoro", "Latte", "Cioccolata", "Pavesini", "Salame", "Macinato misto", "Grissini", "Riso", "Farina", "Zucchero", "Uova", "Sedano", "Insalata", "Salsa di soia"];
+
+//Ciclo di stampa
+for (let i = 0; i < listaSpesa.length; i++) {
+    console.log(i, listaSpesa[i]);
+};
