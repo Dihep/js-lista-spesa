5cdad78131b45b8518ab5fa3473a3f78794927e8
Dihep <dihep.demayo@gmail.com>
2024-04-30 00:07:23 +0200

    M2- Stampa in pagina invece che console (errore nell'ordine di sviluppo).

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 // Creazione variabili
 var listaSpesa = ["Nutella", "Marmellata", "Pane", "Pasta", "Sugo", "Maionese", "Asparagi", "Mascarpone", "Pere", "Mele", "Carote", "Prosciutto", "Jogurt", "Passata di pomodoro", "Latte", "Cioccolata", "Pavesini", "Salame", "Macinato misto", "Grissini", "Riso", "Farina", "Zucchero", "Uova", "Sedano", "Insalata", "Salsa di soia"];
+const lista = document.querySelector("ul");
 
 //Ciclo di stampa
 for (let i = 0; i < listaSpesa.length; i++) {
-    console.log(i, listaSpesa[i]);
+    const newItem = document.createElement("li");
+    newItem.innerText = listaSpesa[i];
+    lista.append(newItem);
 };
